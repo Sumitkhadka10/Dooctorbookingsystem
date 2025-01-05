4f76b13521822be6693b9b4ce001c468569239fb
Sumitkhadka10 <119674989+Sumitkhadka10@users.noreply.github.com>
2025-01-06 00:15:55 +0545

    Created Aout.jsx

---
+++ src/components/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './Header.jsx'; 
+import '../styles/About.css';
+const About = () => {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>HealthCare is dedicated to providing seamless healthcare solutions for everyone.</p>
+    </div>
+  );
+};
+
+export default About;
